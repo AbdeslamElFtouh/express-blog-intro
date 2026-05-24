63c569966b8577bc25a2e455d9f0b1b19cace346
Abdeslam El Ftouh <abdeslamelftouh@gmail.com>
2026-05-24 16:56:17 +0200

    ADD creazione array di post e aggiunta immagini alla cartella public

--- server.js
+++ server.js
@@ -3,6 +3,49 @@ const app = express();
 const serverPort = process.env.SERVER_PORT;
 const serverUrl = process.env.SERVER_URL;
 
+const posts = [
+    {
+        title:'Primo post',
+        content:'Contenuto primo post del blog.',
+        img:'apple.jpg',
+        tags:[
+            'vini', 'vidi' , 'vici'
+        ]
+    },
+    {
+        title:'Secondo post',
+        content:'Contenuto secondo post del blog.',
+        img:'sunrise.jpg',
+        tags:[
+            'divide', 'et' , 'impera'
+        ]
+    },
+    {
+        title:'Terzo post',
+        content:'Contenuto terzo post del blog.',
+        img:'apple.jpg',
+        tags:[
+            'alea', 'iacta' , 'est'
+        ]
+    },
+    {
+        title:'Quarto post',
+        content:'Contenuto quarto post del blog.',
+        img:'sunrise.jpg',
+        tags:[
+            'memento', 'amori' 
+        ]
+    },
+    {
+        title:'Quinto post',
+        content:'Contenuto quinto post del blog.',
+        img:'apple.jpg',
+        tags:[
+            'audentes', 'fortuna' , 'iuvat'
+        ]
+    }
+]
+
 app.get('/' , (request , response) => {
     response.send('<h1>Server del mio blog</h1>')
 })
